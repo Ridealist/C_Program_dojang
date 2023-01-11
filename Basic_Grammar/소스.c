853ea3988790a63535cde3ed0c47a4dd7cdb6ca6
Ridealist <cjsn92@gmail.com>
2023-01-11 15:36:26 +0900

    TIL: 비트연산 및 입력 기능 연습

---
+++ Basic_Grammar/소스.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    int num1;
+    float float1;
+    char char1;
+
+    scanf("%d %f %c", &num1, &float1, &char1);
+    printf("%d\n%f\n%c", num1, float1, char1);
+
+    return 0;
+}
