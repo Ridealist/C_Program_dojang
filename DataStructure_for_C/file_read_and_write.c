1decbc0c674fcda30ea79a6458a9ed33180e236e
리디 <ridealist@junebug-MacBookPro.local>
2023-01-17 22:58:07 +0900

    TIL: C로_배우는_자료구조_3강 수강 완료

--- DataStructure_for_C/file_read_and_write.c
+++ DataStructure_for_C/file_read_and_write.c
@@ -5,7 +5,7 @@ int main()
     FILE * in_fp = fopen("input.txt", "r");
     FILE * out_fp = fopen("output.txt", "w");
     char buffer[100];
-    while (fscanf(in_fp, "%s", buffer) != EOF)  
+    while (fscanf(in_fp, "%s", buffer) != EOF)   
     {
         fprintf(out_fp, "%s ", buffer);
     }
